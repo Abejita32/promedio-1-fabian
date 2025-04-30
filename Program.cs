62b28fe9d5d558afb0e333b3b3fef3d3470a0d5d
fabian <fabiangalvezcueto2004@gmail.com>
2025-04-29 23:54:13 -0500

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+               int papas=Int32.Parse(Console.ReadLine());
+               int pollo=Int32.Parse(Console.ReadLine());
+                
+             jugador papa=new jugador(papas,pollo);
+             int perro=Int32.Parse(Console.ReadLine());
+                List<enemigo> lista = new List<enemigo>();
+
+                for (int i = 0; i < papas; i++)
+                {
+                    
+                  int zorro=Int32.Parse(Console.ReadLine());
+                  int gato=Int32.Parse(Console.ReadLine());
+                   enemigo yuca=new enemigo(gato,perro);
+                    lista.Add(yuca);
+                }
+
+
+            }
+        }
+    }
+}
---
+++ enemigo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    internal class enemigo
+    {
+      private int vida;
+      private int daño;
+        public enemigo(int vida,int daño)
+        {
+            this.daño = daño;
+            this.vida = vida;
+        
+        }
+    }
+}
